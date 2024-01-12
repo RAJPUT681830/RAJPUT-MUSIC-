b3673b1b94ba7310702dffaf09b93a4483c2cd91
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 13:19:03 +0530

    Update language.py

--- RAJPUT/utils/decorators/language.py
+++ RAJPUT/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import get_lang, is_maintenance
+from RAJPUT.misc import SUDOERS
+from RAJPUT.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
