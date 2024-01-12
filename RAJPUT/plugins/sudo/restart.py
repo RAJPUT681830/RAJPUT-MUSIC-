9e8e787d8527e909f9ac9ad6988af4bbb4ee1af3
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 13:46:38 +0530

    Update restart.py

--- RAJPUT/plugins/sudo/restart.py
+++ RAJPUT/plugins/sudo/restart.py
@@ -10,15 +10,15 @@ from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
 
 import config
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import HAPP, SUDOERS, XCB
-from DAXXMUSIC.utils.database import (
+from RAJPUT import app
+from RAJPUT.misc import HAPP, SUDOERS, XCB
+from RAJPUT.utils.database import (
     get_active_chats,
     remove_active_chat,
     remove_active_video_chat,
 )
-from DAXXMUSIC.utils.decorators.language import language
-from DAXXMUSIC.utils.pastebin import DAXXBin
+from RAJPUT.utils.decorators.language import language
+from RAJPUT.utils.pastebin import DAXXBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
