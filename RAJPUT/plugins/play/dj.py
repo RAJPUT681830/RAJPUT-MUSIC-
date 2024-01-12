62cd79f80848ef63d6dfd0f24c8fb56c141ea18a
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 10:55:15 +0530

    Add files via upload

--- RAJPUT/plugins/play/dj.py
+++ RAJPUT/plugins/play/dj.py
@@ -5,7 +5,7 @@ import os
 from DAXXMUSIC import app
 
 
-@app.on_message(filters.command("bass"))
+@app.on_message(filters.command("dj"))
 async def bass_boost_command(client, message):
     try:
         # Check if there is a reply to the command
@@ -36,7 +36,7 @@ def apply_bass_boost(audio_path):
     audio = AudioSegment.from_file(audio_path)
 
     # Apply bass boost effect (adjust the gain according to your preference)
-    boosted_audio = audio.low_pass_filter(100).high_pass_filter(30).apply_gain(10)
+    boosted_audio = audio.low_pass_filter(200).high_pass_filter(70).apply_gain(30)
 
     # Save the boosted audio as a temporary file
     boosted_audio_path = "yumiboost.mp3"
