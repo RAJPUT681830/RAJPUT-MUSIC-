ea4713138fc3e0c8a8f33a66c4f46c0152647a1c
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 13:15:36 +0530

    Update queue.py

--- RAJPUT/utils/inline/queue.py
+++ RAJPUT/utils/inline/queue.py
@@ -1,6 +1,6 @@
 from typing import Union
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from RAJPUT import app
+from RAJPUT.utils.formatters import time_to_seconds
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
