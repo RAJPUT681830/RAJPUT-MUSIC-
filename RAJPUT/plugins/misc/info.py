a04793e4294a0332c46a07c0d74e0fb0eb95b218
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 14:02:47 +0530

    Update info.py

--- RAJPUT/plugins/misc/info.py
+++ RAJPUT/plugins/misc/info.py
@@ -2,7 +2,7 @@ import asyncio, os, time, aiohttp
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 from asyncio import sleep
-from DAXXMUSIC import app as Hiroko
+from RAJPUT import app as Hiroko
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from pyrogram.types import *
@@ -59,8 +59,8 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "DAXXMUSIC/assets/userinfo.png"
-font_path = "DAXXMUSIC/assets/hiroko.ttf"
+bg_path = "RAJPUT/assets/userinfo.png"
+font_path = "RAJPUT/assets/hiroko.ttf"
 
 # --------------------------------------------------------------------------------- #
 
