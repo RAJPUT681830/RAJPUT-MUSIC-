98dbc3f5bf192a683541165dd9d010b777c76dea
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 13:22:26 +0530

    Update welcome.py

--- RAJPUT/plugins/tools/welcome.py
+++ RAJPUT/plugins/tools/welcome.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from DAXXMUSIC import app
+from RAJPUT import app
 
 
 
@@ -118,7 +118,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
     except AttributeError:
-        pic = "DAXXMUSIC/assets/upic.png"
+        pic = "RAJPUT/assets/upic.png"
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
