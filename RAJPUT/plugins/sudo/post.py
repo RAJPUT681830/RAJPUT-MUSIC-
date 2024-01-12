03611804cbf125e0fe7b55e96eff4eee63b79a6b
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-12 13:47:56 +0530

    Update post.py

--- RAJPUT/plugins/sudo/post.py
+++ RAJPUT/plugins/sudo/post.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from DAXXMUSIC import app
+from RAJPUT import app
 from config import OWNER_ID, BOT_USERNAME
 from pyrogram.types import Message
 
@@ -9,7 +9,7 @@ async def copy_messages(_, message):
 
     if message.reply_to_message:
       
-        destination_group_id = -1001927107785
+        destination_group_id = -1001918384430
  
 
         
